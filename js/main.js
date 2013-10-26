9a444cd8ce99ae49b7a73b28d267ec3a9cd24093
G.Gumzakov <ggamzakov@gmail.com>
2013-10-27 01:17:07 +0300

    Merge test

---
+++ js/main.js
@@ -0,0 +1,27 @@
+window.onload = initAll;
+
+function initAll(){
+	$.getJSON( "js/menu.json", function( data ) {
+		alert("success!");
+	});
+}
+
+function parseMenu(map,path){
+	if (path !== ""){
+		var subpath = "";
+		for(var i = 0; i < path.length; i++)
+		{
+			if(path[i] === "/")
+			{
+				subpath = path.substring(0,i);
+				path = path.substring(i+1);
+				break;
+			}
+		}
+		return parseMenu(map[subpath],path);
+	}else{
+		return map;
+	}
+}
+
+
